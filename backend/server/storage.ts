de28d45b9070b64c0646c21769886fa9ea2e0c5d
MikeWhite-ZP <168873774+MikeWhite-ZP@users.noreply.github.com>
2025-11-28 00:21:39 -0600

    Update storage.ts

--- backend/server/storage.ts
+++ backend/server/storage.ts
@@ -68,7 +68,7 @@ import {
   emailTemplates,
   type EmailTemplate,
   type InsertEmailTemplate,
-} from from "../../../shared/schema";
+} from "../../../shared/schema";
 import { db } from "./db";
 import { eq, and, desc, like, sql } from "drizzle-orm";
 import bcrypt from "bcrypt";
